50d68ce88d4262f9b02cc59128f595d386d7baa0
Proxy <flapbraga@gmail.com>
2024-05-26 23:00:31 -0300

    Alteration layout

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -12,6 +12,26 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:type" content="website" />
+        <meta name="og:site_name" content="Cheetah Solucions"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#7289da" />
+        <meta property="og:image" content="/images/terms-en.png" />
+        <meta
+          name="description"
+          content="Hello and welcome to the Cheetah Solutions Network! We specialize in creating communities for Discord, developing dates for the SA:MP community, editing videos, creating custom websites and unique bots. We are ready to meet your needs and strengthen your online community."
+        />
+        <link rel="icon" href="/images/logotipo.ico" type="image/x-icon" />
+        <link 
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" 
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+      </head>
       <body>
         {children}
       </body>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:type" content="website" />
+        <meta name="og:site_name" content="Cheetah Solucions"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#7289da" />
+        <meta property="og:image" content="/images/terms-en.png" />
+        <meta
+          name="description"
+          content="Olá e bem-vindo à Cheetah Solutions Network! Somos especializados em criar comunidades para Discord, desenvolver datas para a comunidade de SA:MP, editar vídeos, criar sites personalizados e bots exclusivos. Estamos prontos para atender às suas necessidades e fortalecer sua comunidade online."
+        />
         <link rel="icon" href="/images/logotipo.ico" type="image/x-icon" />
         <link 
           rel="stylesheet"
@@ -27,6 +36,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
